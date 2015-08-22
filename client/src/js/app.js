1a9dc64d612fac64ec7a606537a5ab2603e5512c
Cody Seibert <cseibert113@gmail.com>
2015-08-22 02:56:32 -0400

    - ui and backend connected

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -14,20 +14,20 @@ $.ajax({
   type: 'GET',
   success: function(urls) {
 
-    var nodes = {};
+    var nodeMap = {};
     var links = [];
 
     for (var i in urls) {
       var url = urls[i];
-      nodes[url.id] = url;
+      nodeMap[url.id] = url;
     }
 
-    for (var key in nodes) {
-      var val = nodes[key]
+    for (var key in nodeMap) {
+      var val = nodeMap[key]
       if (val.target !== -1) {
         links.push({
-          source: nodes[val.id],
-          target: nodes[val.target]
+          source: nodeMap[val.id],
+          target: nodeMap[val.target]
         });
       }
     }
@@ -39,94 +39,109 @@ $.ajax({
         height = 500;
 
     var force = d3.layout.force()
-        .nodes(d3.values(nodes))
-        .links(links)
         .size([width, height])
+        .nodes(d3.values(nodeMap))
+        .links(links)
         .linkDistance(50)
         .charge(-200)
-        .on("tick", tick)
-        .start();
+        .on("tick", tick);
 
     var svg = d3.select("body").append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    var link = null;
-    var node = null;
+    var nodes = force.nodes(),
+        links = force.links(),
+        node = svg.selectAll(".node"),
+        link = svg.selectAll(".link");
 
-    var link = svg.selectAll(".link")
-        .data(force.links())
-        .enter().append("line")
+    restart();
+
+    function restart() {
+      link = link.data(links);
+
+      link
+        .enter()
+        .append("line")
         .attr("class", "link");
-        
-    var node = svg.selectAll(".node")
-        .data(force.nodes())
-        .enter().append("g")
-        .on("click", function (node) {
-          clickCount++;
-          setTimeout(function() {
-            if (clickCount === 1) {
-              console.log('1');
-              selected = node;
-              $('#url').val(node.url);
-              $('#name').val(node.name);
-            } else if (clickCount === 2) {
-              console.log('2');
-              dblclick = true;
-              window.open(node.url, '_blank');
-            } else if (clickCount === 3) {
-              console.log('3');
-              $.ajax({
-                url: 'http://localhost:5001/urls',
-                type: 'POST',
-                data: {
-                  name: 'LOCALHOST',
-                  url: 'http://localhost/',
-                  target: node.id
-                },
-                success: function(created) {
-                  console.log(created);
-                  nodes[created.id] = created
-                  links.push({
-                    target: created.id,
-                    source: node.id
-                  });
-                }
-              });
-            }
-            clickCount = 0;
-          }, 300);
-        })
-        .attr("class", "node");
 
-    var texts = node.append("text")
-            .attr("x", 24)
-            .attr("dy", ".35em")
-            .text(function(d) { return d.name; });
+      node = node.data(nodes);
 
-    var circles = node.append("circle")
+      var g = node
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("r", 5)
+        .on("click", clicked)
+        .call(force.drag);
+
+      g
+        .append("circle")
         .attr("r", 10);
 
-    function update() {
-      texts.text(function(d) { return d.name; });
+      g
+        .append("text")
+        .attr("x", 24)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.name; });
+
+      node = svg.selectAll(".node");
+
+      force.start();
     }
-    update();
+
+    function clicked(node) {
+      clickCount++;
+
+      setTimeout(function() {
+        if (clickCount === 1) {
+          selected = node;
+          $('#url').val(node.url);
+          $('#name').val(node.name);
+        } else if (clickCount === 2) {
+          dblclick = true;
+          window.open(node.url, '_blank');
+        } else if (clickCount === 3) {
+          $.ajax({
+            url: 'http://localhost:5001/urls',
+            type: 'POST',
+            data: {
+              name: 'LOCALHOST',
+              url: 'http://localhost/',
+              target: node.id
+            },
+            success: function(created) {
+              force.nodes().push(created);
+              nodeMap[created.id] = created;
+              force.links().push({
+                target: nodeMap[created.id],
+                source: nodeMap[node.id]
+              });
+              restart();
+            }
+          });
+        }
+        clickCount = 0;
+      }, 400);
+    }
 
     function tick() {
       link
-          .attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
       node
-          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+        // .attr("x", function(d) { return d.x; })
+        // .attr("y", function(d) { return d.y; });
     }
 
     $('#name').keyup(function() {
       if (selected === null) return;
       selected.name = $(this).val();
-      update();
+      refresh();
 
       $.ajax({
         url: 'http://localhost:5001/urls/' + selected.id,
@@ -139,7 +154,7 @@ $.ajax({
     $('#url').keyup(function(){
       if (selected === null) return;
       selected.url = $(this).val();
-      update();
+      refresh();
 
       $.ajax({
         url: 'http://localhost:5001/urls/' + selected.id,
